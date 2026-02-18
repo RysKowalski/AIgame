9946b44013120e736651e0c56beaaed6e3aecc28
RysKowalski <ryskowalski0@gmail.com>
2026-02-18 20:17:31 +0100

    updates to script

--- script_engine.py
+++ script_engine.py
@@ -1,21 +1,17 @@
-"""
-at the start, script engine will load variables from level
-$1 $2 $3 $4 ...
-and special variables
-$count - count of level variables
-$time - current time of the level
-$reward - current reward
+from dataclasses import dataclass
 
-at the start of execution engine creates special variable $return
-$return - value returned by execution
-default value is 0
-when running as a single line, $return is ignored and instead expression is used directly
 
-variables can be created by writing
-variableName = <number|expression>
-then variables can be used by using $variableName
+@dataclass(frozen=True)
+class ScriptSquareData:
+    x: float
+    y: float
+    width: float
+    height: float
+    rotation: float
+    backgroundColor: tuple[float, float, float]
+    borderWidth: float
+    borderColor: tuple[float, float, float]
 
-expressions
-expressions can be used like this:
-$return = ($var1 + $var2) * var3
-"""
+class ScriptEngine:
+    def __init__(self) -> None:
+        
