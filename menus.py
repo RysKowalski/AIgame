a21695264bd68e67a8eb3eaaca906310a72a3446
RysKowalski <ryskowalski0@gmail.com>
2026-03-03 14:10:10 +0100

    continue

--- menus.py
+++ menus.py
@@ -2,7 +2,30 @@ import pygame
 from game_objects import GameObject
 
 
-def add_element_menu(screen: pygame.Surface, elementList: list[GameObject]):
-    running: bool = True
-    while running:
-        pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(12, 12, 12, 12))
+class AddElementMenu:
+    def __init__(
+        self,
+        screen: pygame.Surface,
+        gameObjectList: list[GameObject],
+        font: pygame.font.Font,
+        elements: dict[str, GameObject],
+    ) -> None:
+        self.screen: pygame.Surface = screen
+        self.gameObjectList: list[GameObject] = gameObjectList
+        self.font: pygame.font.Font = font
+        self.elements: dict[str, GameObject] = elements
+        self.visible: bool = False
+
+    def show(self) -> None:
+        self.visible = True
+
+    def hide(self) -> None:
+        self.visible = False
+
+    def process_event(self, event: pygame.event.Event) -> None:
+        if not self.visible:
+            return
+
+    def draw(self) -> None:
+        if not self.visible:
+            return
