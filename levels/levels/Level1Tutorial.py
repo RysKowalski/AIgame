4ac4b540765c536d57ceb0122fe8f45628af678d
RysKowalski <ryskowalski0@gmail.com>
2026-05-11 12:01:25 +0200

    creating structure visualization

--- levels/levels/Level1Tutorial.py
+++ levels/levels/Level1Tutorial.py
@@ -1,22 +1,6 @@
 from levels import GameLevel, LevelData, LevelState
 
 
-class Level1TutorialTemporary(GameLevel):
-    def __init__(self) -> None:
-        super().__init__(LevelData("tutorial1", 1, 100))
-
-    def tick(self, state: LevelState, inputs: tuple[float, ...]) -> None:
-        input: float = self.process_input(inputs)
-        state.variables[0] += input
-        state.variables[1] -= input / 2
-        state.reward = sum(state.variables)
-        self.check_and_set_end(state)
-
-    def process_input(self, inputs: tuple[float, ...]) -> float:
-        maxValue: float = 10
-        return min(inputs[0], maxValue)
-
-
 class Level1Tutorial(GameLevel):
     def __init__(self) -> None:
         super().__init__(LevelData("tutorial1", 1, 100))
--- levels/levels/Level1TutorialTemporary.py
+++ levels/levels/Level1TutorialTemporary.py
@@ -6,12 +6,12 @@ class Level1TutorialTemporary(GameLevel):
         super().__init__(LevelData("tutorial1", 1, 100))
 
     def tick(self, state: LevelState, inputs: tuple[float, ...]) -> None:
-        input: float = self.process_input(inputs)
+        input: float = self._process_input(inputs)
         state.variables[0] += input
         state.variables[1] -= input / 2
         state.reward = sum(state.variables)
         self.check_and_set_end(state)
 
-    def process_input(self, inputs: tuple[float, ...]) -> float:
+    def _process_input(self, inputs: tuple[float, ...]) -> float:
         maxValue: float = 10
         return min(inputs[0], maxValue)
